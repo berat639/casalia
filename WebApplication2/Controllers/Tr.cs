cc5e2eeb30c8a9a6365d060482d77b7442c66fc1
Mona Lisa <mona.lisa@berkee.com>
2024-09-15 22:14:24 +0300

    test

--- WebApplication2/Controllers/Tr.cs
+++ WebApplication2/Controllers/Tr.cs
@@ -8,13 +8,99 @@ namespace WebApplication2.Controllers
         {
             return View();
         }
-		public IActionResult TinyHouse()
-		{
-			return View();
-		}
 		public IActionResult TinyHouseDetay()
 		{
 			return View();
 		}
+		[Route("tr/otomatik-paketleme-rulolari")]
+		public IActionResult OtomatikPaketlemeRulolari()
+		{
+			return View();
+		}
+
+		// MİKRO DELİKLİ AMBALAJ
+		[Route("tr/mikro-delikli-ambalaj")]
+		public IActionResult MikroDelikliAmbalaj()
+		{
+			return View();
+		}
+
+		// SEBZE - MEYVE TORBALARI
+		[Route("tr/sebze-meyve-torbalari")]
+		public IActionResult SebzeMeyveTorbalari()
+		{
+			return View();
+		}
+
+		// ÇİÇEK TORBALARI
+		[Route("tr/cicek-torbalari")]
+		public IActionResult CicekTorbalari()
+		{
+			return View();
+		}
+
+		// KAĞIT ÇİÇEK TORBALARI
+		[Route("tr/kagit-cicek-torbalari")]
+		public IActionResult KagitCicekTorbalari()
+		{
+			return View();
+		}
+
+		// EKMEK TORBALARI
+		[Route("tr/ekmek-torbalari")]
+		public IActionResult EkmekTorbalari()
+		{
+			return View();
+		}
+
+		// KASA FÖYLERİ
+		[Route("tr/kasa-foyleri")]
+		public IActionResult KasaFoyleri()
+		{
+			return View();
+		}
+
+		// KARGO TORBALARI
+		[Route("tr/kargo-torbalari")]
+		public IActionResult KargoTorbalari()
+		{
+			return View();
+		}
+
+		// PETEK KORUYUCULU KARGO TORBALARI
+		[Route("tr/petek-koruyuculu-kargo-torbalari")]
+		public IActionResult PetekKoruyuculuKargoTorbalari()
+		{
+			return View();
+		}
+
+		// TEKSTİL TORBALARI
+		[Route("tr/tekstil-torbalari")]
+		public IActionResult TekstilTorbalari()
+		{
+			return View();
+		}
+
+		// ÇÖP TORBALARI
+		[Route("tr/cop-torbalari")]
+		public IActionResult CopTorbalari()
+		{
+			return View();
+		}
+
+		// GÜVENLİK TORBALAR
+		[Route("tr/guvenlik-torbalari")]
+		public IActionResult GuvenlikTorbalari()
+		{
+			return View();
+		}
+
+		// PE SHRINK FILM
+		[Route("tr/pe-shrink-film")]
+		public IActionResult PeShrinkFilm()
+		{
+			return View();
+		}
+
 	}
 }
